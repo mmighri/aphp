f869b873d4955d35d75302d5d9e4a5f784d3922c
Marouane <mmighri@fr.ibm.com>
2017-11-05 20:14:40 +0100

    init

---
+++ APP/APHPPortalBE/src/main/java/fr/aphp/portal/be/Test.java
@@ -0,0 +1,30 @@
+package fr.aphp.portal.be;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker/hello")
+public class Test {
+
+	@Bean
+	public DataSource getDatasource() {
+		// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).build();
+		return db;
+	}
+
+	@GetMapping
+	public String sayHello() {
+		return "HELLO MF, IS THIS REAL LIFE ?";
+	}
+
+}
---
+++ APP/APHPPortalBE/src/test/java/fr/aphp/portal/be/PortalBeApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.aphp.portal.be;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortalBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
