6049755254baa973281fce66b1af34bc4803013e
Marouane <mmighri@fr.ibm.com>
2017-11-05 20:14:40 +0100

    init

---
+++ APP/APHPPortalBE/src/main/java/fr/aphp/portal/PortalBE/PortalBeApplication.java
@@ -0,0 +1,15 @@
+package fr.aphp.portal.PortalBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class PortalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalBeApplication.class, args);
+	}
+}
